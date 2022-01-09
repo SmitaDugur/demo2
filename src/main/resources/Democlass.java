a7a0a5aa335ff37020cd399234af63bb14561065
SmitaDugur <smitadugur25@gmail.com>
2022-01-09 15:51:52 +0530

    ADDED:initial commit

---
+++ src/main/resources/Democlass.java
@@ -0,0 +1,14 @@
+package main.resources;
+
+public class Democlass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+hkj'jkjbb
+hjlk
+nknkk
+s
---
+++ src/test/resources/projects/Testclass.java
@@ -0,0 +1,10 @@
+package test.resources.projects;
+
+public class Testclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}fkglh;j;ouplyhp;ujpjk
